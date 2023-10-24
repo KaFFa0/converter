3482d69cf4a9fb048630682ce5101ffa86f27d02
kaffa <kaffa991@mail.ru>
2023-10-24 15:57:40 +0300

    prettier code

--- convert.py
+++ convert.py
@@ -9,4 +9,5 @@ soup =  BeautifulSoup(content.text,'html.parser')
 currencies = soup.find_all('tr')
 
 for cur in currencies[1:]:
-    print(cur.text)
+    n = len(cur.text)
+    print(f'{cur.text[4:8]}\n{cur.text[9]} {cur.text[11:n-1]} rubles')
